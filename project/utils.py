d031ca15a6c79da1def803cb3a02ddc104240d84
nikhilpadal <nikhilraj86@gmail.com>
2020-03-22 16:50:55 +0530

    Update utils.py

--- project/utils.py
+++ project/utils.py
@@ -49,9 +49,15 @@ def load_embeddings(embeddings_path):
     ########################
     #### YOUR CODE HERE ####
     ########################
-
+    embeddings = dict()
+    for line in open(embeddings_path, encoding='utf-8'):
+        row = line.strip().split('\t')
+        embeddings[row[0]] = np.array(row[1:], dtype=np.float32)
+    embeddings_dim = embeddings[list(embeddings)[0]].shape[0]
+    
+    return embeddings, embeddings_dim
     # remove this when you're done
-    raise NotImplementedError(
+    #raise NotImplementedError(
         "Open utils.py and fill with your code. In case of Google Colab, download"
         "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
         "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
